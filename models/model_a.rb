d81dd5b7603a92b94a60f1ec2a23ef2ed55236ba
Eldridge Turambi <eturambi95@gmail.com>
2021-07-30 17:05:25 -0600

    Add new hash for turing laptop object

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+turing_laptop = {
+  "laptop_name" => "Betsy",
+  "laptop_color" => "Space Gray",
+  "number_of_screens" => 1,
+  "number_of_buttons" => 80,
+  "is_fast" => true,
+  "is_slow" => false,
+  "key_programs" => ["Terminal", "Atom", "Google Chrome"],
+  "wallpaper_types" =>["Mountain", "Beach", "Abstract"]
+
+}
