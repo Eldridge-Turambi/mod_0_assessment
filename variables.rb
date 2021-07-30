a762476c022e1fbaa6ad96208189b180c451064c
Eldridge Turambi <eturambi95@gmail.com>
2021-07-30 16:52:29 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+anmial = "bear"
+is_cuddly = true
+anmial = "dog"
+num_8 = 50
+num_3 = 100
+quotient = "#{num_3 / num_8}"
+favorite_films = ["Nacho Libre", "Scott Pilgrim", "The Dark Knight"]
+
+# Deletes an input in an array (0 being the first one noted => "Nacho Libre")
+favorite_films.delete_at (0)
+
+# Prints the amount of charachters in the "anmial" string
+puts anmial.length
