9ff03fff0ee32296f33e732d02db1fd992a3dbf8
Eldridge Turambi <eturambi95@gmail.com>
2021-07-30 17:14:51 -0600

    Add new hash for 'house' object

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+my_house = {
+  "house_size" => "Big",
+  "house_name" => "Tansy House",
+  "number_of_rooms" => 4,
+  "number_of_flowers" => 24,
+  "is_house_far" => true,
+  "is_house_tall" => false,
+  "people_living_inside" => ["Ellen", "David", "Eldridge", "Denzel", "Tim"],
+  "appliances" => ["Fridge", "Oven", "Washer", "Dryer"]
+}
